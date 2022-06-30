9c7fcfa52d06b42f8c8d608dfed100093fdffeaa
boosko80 <boosko80@gmail.com>
2022-06-30 15:35:49 +0200

    EBP-2 Added StyledButton

---
+++ src/components/BaseButton.tsx
@@ -0,0 +1,41 @@
+import styled, { css } from "styled-components";
+
+interface IProps {
+  disabled?: boolean;
+}
+
+const StyledButton = styled.button<IProps>`
+  cursor: pointer;
+
+  padding: 0.75rem 1.5rem;
+  
+  border-radius: 0.25rem;
+  border-width: 0rem;
+
+  box-shadow: 0.125rem 0.125rem 0.5rem gray;
+
+  background-color: #2563eb;
+
+  font-weight: bold;
+  color: white;
+
+  &:hover, :focus {
+    background-color: #1e40af;
+  }
+
+  ${props => 
+    props.disabled && css`
+      cursor: not-allowed;
+
+      opacity: 0.5;
+
+      background-color: #737373;
+
+      &:hover {
+        background-color: #737373;
+      }
+    `
+  }
+`
+
+export { StyledButton };
