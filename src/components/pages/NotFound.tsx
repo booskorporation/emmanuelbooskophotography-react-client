65e9d3d802db63951ca45c8275749b499f300b1f
boosko80 <boosko80@gmail.com>
2022-07-04 09:13:34 +0200

    EBP-2 Added 404 Page

---
+++ src/components/pages/NotFound.tsx
@@ -0,0 +1,12 @@
+import { MainHeader } from '../MainHeader';
+
+function NotFound() {
+  return (
+    <>
+      <MainHeader />
+      <div>This is the 404 page</div>
+    </>
+  );
+}
+
+export { NotFound };
