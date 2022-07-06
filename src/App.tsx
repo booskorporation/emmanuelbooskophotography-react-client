90e03ff565a1462528ff284446bdd0da79c41042
boosko80 <boosko80@gmail.com>
2022-07-06 18:49:50 +0200

    EBP-8 Moved BrowserRouter to App and Grouped exports for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import { HomePage } from './pages/HomePage';
-import { AlbumsPage } from './pages/AlbumsPage';
-import { AgendaPage } from './pages/AgendaPage';
-import { AboutPage } from './pages/AboutPage';
-import { ContactPage } from './pages/ContactPage';
-import { NotFoundPage } from './pages/NotFoundPage';
+import { HomePage, AlbumsPage, AgendaPage, AboutPage, ContactPage, NotFoundPage } from "./pages"
 
-function App() {
-  return (
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/albums' element={<AlbumsPage />} />
-        <Route path='/agenda' element={<AgendaPage />} />
-        <Route path='/about' element={<AboutPage />} />
-        <Route path='/contact' element={<ContactPage />} />
-        <Route path='*' element={<NotFoundPage />} />
-      </Routes>
-  );
-}
+const App = (): JSX.Element => (
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/albums' element={<AlbumsPage />} />
+      <Route path='/agenda' element={<AgendaPage />} />
+      <Route path='/about' element={<AboutPage />} />
+      <Route path='/contact' element={<ContactPage />} />
+      <Route path='*' element={<NotFoundPage />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,6 @@
+export { AboutPage } from "./AboutPage";
+export { AgendaPage } from "./AgendaPage";
+export { AlbumsPage } from "./AlbumsPage";
+export { ContactPage } from "./ContactPage";
+export { HomePage } from "./HomePage";
+export { NotFoundPage } from "./NotFoundPage";
