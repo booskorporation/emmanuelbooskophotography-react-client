0b5e892f617f0f78b5cb184a4ae83422f600d393
boosko80 <wesley.boosko@gmail.com>
2022-07-07 14:35:22 +0200

    EBP-5 JSX only in tsx files

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,7 +34,7 @@ module.exports = {
   },
   rules: {
     'react/react-in-jsx-scope': 'off',
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', 'ts', 'tsx'] }],
+    'react/jsx-filename-extension': [1, { extensions: ['tsx'] }],
     'react/function-component-definition': [2, { namedComponents: 'arrow-function', unnamedComponents: 'arrow-function' }],
     'import/prefer-default-export': 'off',
     'import/extensions': 'off',
