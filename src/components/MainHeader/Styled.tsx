84a0b27cc8f2d8c3be0359f38087f13afd39f446
boosko80 <boosko80@gmail.com>
2022-07-11 18:54:18 +0200

    EBP-9 Added css variables

--- src/components/MainHeader/Styled.tsx
+++ src/components/MainHeader/Styled.tsx
@@ -5,13 +5,14 @@ const StyledHeader = styled.header`
   position: sticky;
   top: 0;
   z-index: 100;
+  background-color: var(--fg);
 `;
 
 const StyledHeaderTitle = styled.h1`
-  margin: 0;
-  background-color: #e2e8f0;
-  font-size: 4rem;
+  padding-block: var(--step--1);
+  font-size: var(--step-3);
   text-align: center;
+  font-weight: bold;
 `;
 
 export { StyledHeader, StyledHeaderTitle };
