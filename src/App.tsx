6869a49da7c62369b85000c358f371de40da9978
boosko80 <boosko80@gmail.com>
2022-07-11 16:15:52 +0200

    EBP-11 Added GlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import { GlobalStyle } from './styles/GlobalStyle';
 import {
   HomePage, AlbumsPage, AlbumPage, AgendaPage, AboutPage, ContactPage, NotFoundPage,
 } from './pages';
 
 const App = (): JSX.Element => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/albums" element={<AlbumsPage />} />
-      <Route path="/albums/:albumId" element={<AlbumPage />} />
-      <Route path="/agenda" element={<AgendaPage />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/contact" element={<ContactPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
-  </BrowserRouter>
+  <>
+    <GlobalStyle />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/albums" element={<AlbumsPage />} />
+        <Route path="/albums/:albumId" element={<AlbumPage />} />
+        <Route path="/agenda" element={<AgendaPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  </>
 );
 
 export default App;
